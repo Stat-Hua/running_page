c0a310f3f5899030012d0956405e9550de2f92f0
Stat-Hua <125859197+Stat-Hua@users.noreply.github.com>
2025-10-24 16:23:36 +0800

    添加新数据

--- src/components/YearsStat/index.tsx
+++ src/components/YearsStat/index.tsx
@@ -30,9 +30,7 @@ const YearsStat = ({
   return (
     <div className="w-full pb-16 pr-16 lg:w-full lg:pr-16">
       <section className="pb-0">
-        <p className="leading-relaxed">
-          {infoMessage}
-          <br />
+        <p className="leading-relaxed" dangerouslySetInnerHTML={{__html:infoMessage}}>
         </p>
       </section>
       <hr />
